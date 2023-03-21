73c1f6372529eaabffc5d98b6d5fce5c39e08a5b
Nathan Gavin <nathangavin987@gmail.com>
2023-03-21 21:21:16 +1300

    achieved tui where messages can be entered and displayed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,205 @@
-use std::io;
+use std::{io, thread, time::{Duration, Instant}, sync::mpsc, vec};
+use crossterm::{terminal::{enable_raw_mode, EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen}, execute, event::{EnableMouseCapture, DisableMouseCapture, self, KeyEventKind, KeyCode, Event}, style::Stylize};
+use tui::{backend::{CrosstermBackend, Backend}, Terminal, widgets::{Block, Borders, Paragraph, ListItem, List}, layout::{Layout, Direction, Constraint}, Frame, text::{Span, Text, Spans}, style::{Style, Modifier, Color}};
 
-use tui::{backend::CrosstermBackend, Terminal};
+
+enum InputMode {
+    Normal,
+    Editing,
+}
+
+/// App holds the state of the application
+struct App {
+    /// Current value of the input box
+    input: String,
+    /// Current input mode
+    input_mode: InputMode,
+    /// History of recorded messages
+    messages: Vec<String>
+}
+
+impl Default for App {
+    fn default() -> App {
+        App {
+            input: String::new(),
+            input_mode: InputMode::Normal,
+            messages: Vec::new(),
+        }
+    }
+}
+
+/* fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+        .split(f.size());
+
+    println!("{:?}", chunks);
+    let block = Block::default()
+        .title("Block")
+        .borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+/*    let block2 = Block::default()
+        .title("Block 2")
+        .borders(Borders::ALL);
+    f.render_widget(block2, chunks[0]); */
+} */
 
 fn main() -> Result<(), io::Error>{
-    let stdout = io::stdout();
+    enable_raw_mode()?;
+    /*let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(t)
+            }
+        }
+    }); */
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend  = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend);
+    let mut terminal = Terminal::new(backend)?;
+    
+    /* terminal.draw(|f| {
+        ui::<CrosstermBackend<io::Stdout>>(f);
+    })?; */
+
+    // thread::sleep(Duration::from_millis(5000));
+    
+    let app = App::default();
+    let res = run_app(&mut terminal, app);
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+        )?;
+    terminal.show_cursor()?;
+    
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
     Ok(())
 }
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &app))?;
+        if let Event::Key(key) = event::read()? {
+            match app.input_mode {
+                InputMode::Normal => match key.code {
+                    KeyCode::Char('e') => {
+                        app.input_mode = InputMode::Editing;
+                    },
+                    KeyCode::Char('q') => {
+                        return Ok(());
+                    },
+                    _ => {}
+                },
+                InputMode::Editing => match key.code {
+                    KeyCode::Enter => {
+                        app.messages.push(app.input.drain(..).collect());
+                    },
+                    KeyCode::Char(char) => {
+                        app.input.push(char);
+                    },
+                    KeyCode::Backspace => {
+                        app.input.pop();
+                    },
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    },
+                    _ => {}
+                },
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints([
+            Constraint::Length(1),
+            Constraint::Length(3),
+            Constraint::Min(1),
+        ].as_ref())
+        .split(f.size());
+    let (msg, style) = match app.input_mode {
+        InputMode::Normal => (
+            vec![
+                Span::raw("Press "),
+                Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to exit, "),
+                Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to Start editing."),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        ),
+        InputMode::Editing => (
+            vec![
+                Span::raw("Press "),
+                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to stop editing, "),
+                Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to record the message"),
+            ],
+            Style::default(),
+        ),
+    };
+    let mut text = Text::from(Spans::from(msg));
+    text.patch_style(style);
+    let help_message = Paragraph::new(text);
+    f.render_widget(help_message, chunks[0]);
+
+    let input = Paragraph::new(app.input.as_ref())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Editing => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Input"));
+    f.render_widget(input, chunks[1]);
+    match app.input_mode {
+        InputMode::Normal => {},
+        InputMode::Editing => {
+            f.set_cursor(
+                 chunks[1].x + app.input.len() as u16 + 1,
+                 chunks[1].y + 1    
+            )
+        }
+    }
+
+    let messages: Vec<ListItem> = app
+        .messages
+        .iter()
+        .enumerate()
+        .map(|(i, m)| {
+            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+            ListItem::new(content)
+        })
+        .collect();
+    let messages = List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
+    f.render_widget(messages,chunks[2]);
+}
+// https://blog.logrocket.com/rust-and-tui-building-a-command-line-interface-in-rust/
