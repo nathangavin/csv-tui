1b6806b91039d90c09eaf77e92cdccb92cf31151
nathangavin <nathangavin987@gmail.com>
2023-03-25 22:28:06 +1300

    got blinking cell on position saved in app data object

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,8 @@ struct App {
     input: String,
     /// Current input mode
     input_mode: InputMode,
-    /// History of recorded messages
-    messages: Vec<String>,
-
-    data: Vec<Vec<String>>
+    data: Vec<Vec<String>>,
+    pos: (usize, usize)
 }
 
 impl Default for App {
@@ -25,35 +23,11 @@ impl Default for App {
         App {
             input: String::new(),
             input_mode: InputMode::Normal,
-            messages: Vec::new(),
-            data: Vec::new()
+            data: Vec::new(),
+            pos: (0,0)
         }
     }
 }
-
-/* fn ui<B: Backend>(f: &mut Frame<B>) {
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(1)
-        .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10)
-                ].as_ref()
-            )
-        .split(f.size());
-
-    println!("{:?}", chunks);
-    let block = Block::default()
-        .title("Block")
-        .borders(Borders::ALL);
-    f.render_widget(block, chunks[0]);
-/*    let block2 = Block::default()
-        .title("Block 2")
-        .borders(Borders::ALL);
-    f.render_widget(block2, chunks[0]); */
-} */
 
 fn main() -> Result<(), io::Error>{
     enable_raw_mode()?;
@@ -85,8 +59,6 @@ fn main() -> Result<(), io::Error>{
     /* terminal.draw(|f| {
         ui::<CrosstermBackend<io::Stdout>>(f);
     })?; */
-
-    // thread::sleep(Duration::from_millis(5000));
     
     let app = App::default();
     let res = run_app(&mut terminal, app);
@@ -122,7 +94,8 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                 },
                 InputMode::Editing => match key.code {
                     KeyCode::Enter => {
-                        app.messages.push(app.input.drain(..).collect());
+                        //app.messages.push(app.input.drain(..).collect());
+                        todo!();
                     },
                     KeyCode::Char(char) => {
                         app.input.push(char);
@@ -141,6 +114,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
 }
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    // Set up top level page structure
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(0)
@@ -151,80 +125,8 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
                 ].as_ref()
             )
         .split(f.size()); 
-    // println!("{:?}", chunks);
-    /*let grid_chunks: Vec<Vec<Rect>> = chunks
-        .chunks(1)
-        .map(|col| {
-            //println!("{:?}", col);
-            Layout::default()
-                .direction(Direction::Vertical)
-                .margin(0)
-                .constraints([
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Length(3),
-                    Constraint::Min(0),
-                ].as_ref())
-                .split(col[0])
-        }).collect(); */
-    // println!("{:?}",grid_chunks.len());
-    
-    /*for column in grid_chunks {
-        for cell in column {
-            let block = Block::default()
-                //.title("Cell")
-                .borders(Borders::ALL);
-            // f.render_widget(block, cell);
-        }
-    }*/
-    let rows : usize = 50;
-    let cols : usize = 50;
-    let mut table_rows = Vec::new();
-    for _ in 0..rows {
-        let mut row_vec = Vec::new();
-        for _ in 0..cols {
-            row_vec.push(Cell::from("_____"));
-        }
-        table_rows.push(Row::new(row_vec));
-    }
-    
-    let mut widths = Vec::new();
-    for _ in 0..cols {
-        widths.push(Constraint::Length(5));
-    }
 
-    let table = Table::new(table_rows)
-        .header(
-            Row::new(vec!["top1", "top2", "top3"])
-            .style(Style::default().fg(Color::Red))
-            .bottom_margin(1)
-            )
-        .block(Block::default().title("Table").borders(Borders::ALL))
-        .widths(&widths)
-        .column_spacing(1)
-        .highlight_style(Style::default().add_modifier(Modifier::BOLD));
-    f.render_widget(table,chunks[1]);
-    /*let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        // .margin(2)
-        .constraints([
-            Constraint::Length(1),
-            Constraint::Length(3),
-            Constraint::Min(1),
-        ].as_ref())
-        .split(f.size()); */
-    // println!("{:?}", chunks);
-    // println!("{:?}", f.size());
+    // set up dynamic message at top level
     let (msg, style) = match app.input_mode {
         InputMode::Normal => (
             vec![
@@ -252,6 +154,52 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
     let help_message = Paragraph::new(text);
     f.render_widget(help_message, chunks[0]);
 
+    // build table
+    let rows : usize = 50;
+    let cols : usize = 50;
+    let mut table_rows = Vec::new();
+    for row in 0..rows {
+        let mut row_vec = Vec::new();
+        row_vec.push(Cell::from(row.to_string()));
+        for col in 0..cols {
+            match app.input_mode {
+                InputMode::Normal => {
+                    let cell_value = "_____";
+
+
+                    if app.pos.0 == row && app.pos.1 == col {
+                        let style = Style::default().add_modifier(Modifier::RAPID_BLINK);
+                        let cell = Cell::from(Span::styled(cell_value, style));
+                        row_vec.push(cell);
+                    } else {
+                        let cell = Cell::from(cell_value);
+                        row_vec.push(cell);
+                    }
+                    //row_vec.push(cell);
+                }
+                InputMode::Editing => {}
+            }
+            //row_vec.push(Cell::from("_____"));
+        }
+        table_rows.push(Row::new(row_vec));
+    }
+    
+    let mut widths = Vec::new();
+    for _ in 0..cols {
+        widths.push(Constraint::Length(5));
+    }
+
+    let table = Table::new(table_rows)
+        .header(
+            Row::new(vec!["top1", "top2", "top3"])
+            .style(Style::default().fg(Color::Red))
+            .bottom_margin(1)
+            )
+        .block(Block::default().title("Table").borders(Borders::ALL))
+        .widths(&widths)
+        .column_spacing(1)
+        .highlight_style(Style::default().add_modifier(Modifier::BOLD));
+    f.render_widget(table,chunks[1]);
     let input = Paragraph::new(app.input.as_ref())
         .style(match app.input_mode {
             InputMode::Normal => Style::default(),
@@ -268,18 +216,6 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
             )
         }
     }
-
-    let messages: Vec<ListItem> = app
-        .messages
-        .iter()
-        .enumerate()
-        .map(|(i, m)| {
-            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
-            ListItem::new(content)
-        })
-        .collect();
-    let messages = List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-    // f.render_widget(messages,chunks[2]);
 }
 fn cell_border_style() -> Style {
     Style::default().fg(Color::White).bg(Color::Black).add_modifier(Modifier::BOLD)
