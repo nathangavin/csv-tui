e0bc4de1d2fb13676ac4176d07e62d636fe983de
nathangavin <nathangavin987@gmail.com>
2023-04-16 12:25:03 +1200

    added input args, able to load files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, env};
 use tui::{
     Terminal,
     backend::CrosstermBackend
@@ -17,44 +17,22 @@ use crossterm::{
 mod app;
 use app::App;
 
-/// App holds the state of the application
-
 fn main() -> Result<(), io::Error>{
-    enable_raw_mode()?;
-    /*let (tx, rx) = mpsc::channel();
-    let tick_rate = Duration::from_millis(200);
-    thread::spawn(move || {
-        let mut last_tick = Instant::now();
-        loop {
-            let timeout = tick_rate
-                .checked_sub(last_tick.elapsed())
-                .unwrap_or_else(|| Duration::from_secs(0));
-
-            if event::poll(timeout).expect("poll works") {
-                if let CEvent::Key(key) = event::read().expect("can read events") {
-                    tx.send(Event::Input(key)).expect("can send events");
-                }
-            }
-
-            if last_tick.elapsed() >= tick_rate {
-                if let Ok(_) = tx.send(t)
-            }
+    let args: Vec<String> = env::args().collect();
+    let filename = match args.get(1) {
+        Some(name) => name,
+        None => {
+            println!("you must provide a CSV filename as an input arg.");
+            return Ok(());
         }
-    }); */
-
-    //load_file_into_app("./output.csv".to_string()).unwrap();
-    //return Ok(());
+    };
+    enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend  = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     
-    /* terminal.draw(|f| {
-        ui::<CrosstermBackend<io::Stdout>>(f);
-    })?; */
-    
-    //let app = App::default();
-    let app = match App::load_file_into_app(String::from("output.csv")) {
+    let app = match App::load_file_into_app(String::from(filename)) {
         Ok(app) => app,
         Err(_) => {
             disable_raw_mode()?;
@@ -80,5 +58,3 @@ fn main() -> Result<(), io::Error>{
 
 // TODO
 // add commands for inserting/deleting rows & columns
-// add save as feature
-// create input args
